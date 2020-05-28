3ec6b164b0178fe62a4ea5bdfdb092572bc642c5
Germil74 <40329656+Germil74@users.noreply.github.com>
2020-05-28 18:04:06 +0400

    Initial commit

---
+++ gulp/tasks/serve.js
@@ -0,0 +1,21 @@
+module.exports = function() {
+    // $.gulp.task('serve', function() {
+    //     $.browserSync.init({
+    //         server: './public'
+    //     });
+    // });
+
+    $.gulp.task('serve', function() {
+        $.browserSync.init({
+            server: {
+                baseDir: `${$.path.dist}`,
+                serveStaticOptions: {
+                    extensions: ["html"]
+                }
+            },
+            port: 4000,
+            notify: true
+        });
+    });
+
+};
---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+global.$ = {
+    path: {
+        task: require('./gulp/paths/tasks.js'),
+        dist: './public'    //  'D:/host/OSPanel/domains/temp/public'    './public'
+    },
+    gulp: require('gulp'),
+    del: require('del'),
+    fs: require('fs'),
+    webpack: require("webpack-stream"),
+    handlebars: require('handlebars'),
+    browserSync: require('browser-sync').create(),
+    gp: require('gulp-load-plugins')()
+};
+
+$.path.task.forEach(function(taskPath) {
+    console.log(taskPath);
+    require(taskPath)();
+});
+
+$.gulp.task('dev', $.gulp.series(
+    'clean',
+    $.gulp.parallel('scss:dev', 'pug', 'build-js', 'svg', 'img:dev', 'fonts','svg:copy')));
+
+$.gulp.task('build', $.gulp.series(
+    'clean',
+    $.gulp.parallel('scss:build', 'pug', 'build-js', 'svg', 'img:build', 'fonts','svg:copy')));
+
+$.gulp.task('default', $.gulp.series(
+    'dev',
+    $.gulp.parallel(
+        'watch',
+        'serve'
+    )
+));
